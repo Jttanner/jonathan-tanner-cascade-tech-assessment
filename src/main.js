7dccf3a1caba4229b57e171517e8a77166bac340
jonttanner <jonttanner@gmail.com>
2020-09-09 21:02:38 -0600

    get vuex store working to import test data

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
+import TestData from './test-data/transaction-feed-example.json'
+
+console.log(JSON.stringify(TestData))
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    transactionFeed : TestData
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
 new Vue({
   render: h => h(App),
+  store: store,
 }).$mount('#app')
