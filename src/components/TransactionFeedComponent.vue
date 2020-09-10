ac617b6c096c0ea9579736d471f6c9570be7f8c8
jonttanner <jonttanner@gmail.com>
2020-09-10 00:50:58 -0600

    modal display and full top level display

--- src/components/TransactionFeedComponent.vue
+++ src/components/TransactionFeedComponent.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="transaction">
     <h1>{{ title }}</h1>
+    <br />
+  
     <table class="table table-bordered table-striped">
       <thead>
+        <th scope="col">
+          Transaction Date
+        </th>
         <th scope="col">
           Account Number
         </th>
@@ -12,11 +17,20 @@
         <th scope="col">
           Transaction Amount
         </th>
+        <th>
+          Merchant Name
+        </th>
+        <th>
+          Billed
+        </th>
         <th scope="col">
         </th>
       </thead>
       <tbody>
           <tr v-for="transaction in this.$store.state.transactionFeed.Statement.Transactions" v-bind:key="transaction.id">
+            <td>
+              {{new Date(transaction.TransactionDate).toLocaleString()}}
+            </td>
             <td>
               {{transaction.AccountNumber}}
             </td>
@@ -24,13 +38,16 @@
               {{transaction.TransactionTypeId}}
             </td>
             <td>
-              {{transaction.TransactionDate}}
+              ${{transaction.Amount}}
             </td>
             <td>
-              {{transaction.Amount}}
+              {{transaction.MerchantName}}
             </td>
             <td>
-              <button v-on:click="test(transaction)" class="btn btn-info" type="button" data-toggle="collapse" data-target="#detailsPane" aria-expanded="false" aria-controls="collapseExample">
+              {{transaction.Billed ? "Yes" : "Not Settled"}}
+            </td>
+            <td>
+              <button v-on:click="updateModalWithSelectedTransactionData(transaction)" :class="transaction.Billed ? 'btn btn-info' : 'btn btn-warning'" data-toggle="modal" data-target="#selectedTransactionModal">
                 Show Details
               </button>
             </td>
@@ -38,17 +55,136 @@
       </tbody>
     </table>
 
-    <div class="modal" tabindex="-1" role="dialog">
+    <br />
+
+    <table class="table table-bordered table-striped">
+      <thead>
+        <th></th>
+        <th>Ignoring Transactions Not Settled</th>
+        <th>Including Transacions Settled and Not Settled</th>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">Starting Balance</th>
+          <td>${{this.$store.state.startingStatementBalance.toFixed(2)}}</td>
+          <td>${{this.$store.state.startingStatementBalance.toFixed(2)}}</td>
+        </tr>
+        <tr>
+          <th scope="row">Amount Charged</th>
+          <td>-${{this.$store.state.settledTransactionValueSum.toFixed(2)}}</td>
+          <td>-${{(this.$store.state.settledTransactionValueSum + this.$store.state.unsettledTransactionValueSum).toFixed(2)}}</td>
+        </tr>
+        <tr>
+          <th scope="row">Final Balance</th>
+          <td>${{(this.$store.state.startingStatementBalance - this.$store.state.settledTransactionValueSum).toFixed(2)}}</td>
+          <td>{{(this.$store.state.startingStatementBalance - this.$store.state.settledTransactionValueSum - this.$store.state.unsettledTransactionValueSum).toFixed(2)}}</td>
+        </tr>
+      </tbody>
+    </table>
+
+
+    <div class="modal" id="selectedTransactionModal" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title">Modal title</h5>
+            <h5 class="modal-title">Transaction Details</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div class="modal-body">
-            <p>Modal body text goes here.</p>
+            <table class="table table-bordered table-striped">
+              <tbody>
+                <tr>
+                  <th scope="row">
+                    Original Trace Audit Number
+                  </th>
+                  <td>
+                    {{this.$store.state.selectedTransactionData.OriginalTraceAuditNo == null ? "N/A" : this.$store.state.selectedTransactionData.selectedAuditNumber}}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    Related Account Number
+                  </th>
+                  <td>
+                    {{this.$store.state.selectedTransactionData.AccountNumber}}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    Transaction Type
+                  </th>
+                  <td>
+                    {{this.$store.state.selectedTransactionData.TransactionTypeId}}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    Transaction Date
+                  </th>
+                  <td>
+                    {{new Date(this.$store.state.selectedTransactionData.TransactionDate).toLocaleString()}}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    Business Date
+                  </th>
+                  <td>
+                    {{new Date(this.$store.state.selectedTransactionData.BusinessDate).toLocaleString()}}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    Available Balance at Time of Transaction
+                  </th>
+                  <td>
+                    ${{this.$store.state.selectedTransactionData.AvailableBalance}}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    Transaction Amount
+                  </th>
+                  <td>
+                    ${{this.$store.state.selectedTransactionData.Amount}}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    Description
+                  </th>
+                  <td>
+                    {{this.$store.state.selectedTransactionData.Description}}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    Billed
+                  </th>
+                  <td>
+                    {{this.$store.state.selectedTransactionData.Billed ? "Yes" : "Not Settled"}}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    Merchant Name 
+                  </th>
+                  <td>
+                    {{this.$store.state.selectedTransactionData.MerchantName}}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    Merchant Id
+                  </th>
+                  <td>
+                    {{this.$store.state.selectedTransactionData.MerchantId}}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -56,7 +192,7 @@
         </div>
       </div>
     </div>
-    
+
   </div>
 </template>
 
@@ -66,16 +202,44 @@ export default {
   props: {
     title: String
   },
-  methods:{
-    test : function (transactionData){
-      alert(JSON.stringify(transactionData))
-    }
+  created: function() {
+    
+    let earliestDate = null
+    let earliestAvailableBalance = 0;
+    let settledTransactionValueSum = 0;
+    let unsettledTransactionValueSum = 0;
+    this.$store.state.transactionFeed.Statement.Transactions.forEach(function(currTransaction){
+      //get starting balance by getting the available balance from the earliest transaction
+      if (earliestDate == null || currTransaction.TransactionDate < earliestDate){
+        earliestDate = currTransaction.TransactionDate
+        earliestAvailableBalance = currTransaction.AvailableBalance
+      }
+      if (currTransaction.Billed){
+        //Calculate final balance exluding unsettled balances
+        settledTransactionValueSum += currTransaction.Amount
+      }
+      else {
+        //Calculate not settled balance
+        unsettledTransactionValueSum += currTransaction.Amount
+      }
+    })
+    // alert(earliestDate)
+    // alert(earliestAvailableBalance)
+    // alert(settledTransactionValueSum)
+    // alert(unsettledTransactionValueSum)
+    //Update starting balance
+    this.$store.commit('updateStartingStatementBalance', earliestAvailableBalance)
+    //Update final balance 
+    this.$store.commit('updateSettledTransactionValueSum', settledTransactionValueSum)
+    
+    //Update not settled balance
+    this.$store.commit('updateUnsettledTransactionValueSum', unsettledTransactionValueSum)
   },
-  data: function() {
-    return {
-      transactionDataForModal: null //initializes as null, set to the transaction data when "show details" button is clicked
-    }
-  }
+  methods:{
+    updateModalWithSelectedTransactionData : function (transactionData){
+      this.$store.commit('updateSelectedTransactionData', transactionData) //update with selected data so it databinds with modal
+    },
+  },  
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -14,12 +14,25 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    transactionFeed : TestData //for testing this application set the state of the store to our example data
+    transactionFeed : TestData, //for testing this application set the state of the store to our example data
+    selectedTransactionData: {}, //start empty, update when items are selected
+    startingStatementBalance: 0,
+    settledTransactionValueSum: 0,
+    unsettledTransactionValueSum: 0
   },
   mutations: {
-    increment (state) {
-      state.count++
-    }
+    updateSelectedTransactionData (state, selectedTransactionData) {
+      state.selectedTransactionData = selectedTransactionData
+    },
+    updateStartingStatementBalance(state, newStartingStatementBalance){
+      state.startingStatementBalance = newStartingStatementBalance
+    },
+    updateSettledTransactionValueSum(state, newSettledTransactionValueSum){
+      state.settledTransactionValueSum = newSettledTransactionValueSum
+    },
+    updateUnsettledTransactionValueSum(state, newUnsettledTransactionValueSum){
+      state.unsettledTransactionValueSum = newUnsettledTransactionValueSum
+    },
   }
 })
 
